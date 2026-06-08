966780f5542ccf5f7f095b05bb6028cbf2644d15
Tom Thole <tom.thole@ru.nltom.thole@ru.nlhelpgit helpstopgit config user.email tom.thole@student.ru.nl>
2026-06-08 08:56:30 +0200

    beginning class structure

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 # -*- coding: utf-8 -*-
 
+class Controller:
+    
+    def __init__(self, io):
+        self.io = io
+    
+    def app(self):
+        pass
+    
+class InputOutput:
+    def get_gpx(self):
+        input()
 
+def main():
+   con = Controller()
+   io = InputOutput()
+   con.app(io)
+    
+if __name__ == "__main__":
+    main()  
 
