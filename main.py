572e2be7a8d32a7670bc3ed09cd9a1d09da7399e
Tom Thole <tom.thole@ru.nltom.thole@ru.nlhelpgit helpstopgit config user.email tom.thole@student.ru.nl>
2026-06-08 09:12:38 +0200

    added thorougher check of gpx filepath

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import gpxpy
+import re
 
 class Controller:
     
@@ -6,16 +8,30 @@ class Controller:
         self.io = io
     
     def app(self):
-        pass
+        gpx_path = self.io.get_gpx()
     
 class InputOutput:
     def get_gpx(self):
-        input()
+        while True:
+            path = input('Please enter a valid path to a gpx file ')
+            if bool(re.search(r'\.gpx$', path, re.IGNORECASE)):
+                try:
+                    open(path)    
+                    return path
+                except:
+                    print('Invalid path provided')
+            else:
+                print('No GPX file provided ')
+            
+class GPXHandling:
+    def __init__(self, path):
+        self.path = path
+            
 
 def main():
-   con = Controller()
    io = InputOutput()
-   con.app(io)
+   con = Controller(io)
+   con.app()
     
 if __name__ == "__main__":
     main()  
